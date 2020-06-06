f055b2e95e808b3d5af1281ce326e6869a0b5cf2
g-kelvin <kelvingauki@gmail.com>
2020-06-06 22:31:25 +0300

    social platforms update

---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+    $con=mysqli_connect("localhost","root","","profwaititu");
+
+    if($con){
+        $email = $_POST['email'];
+        $pass = $_POST['pass'] ;    
+        echo $pass;
+        $qry="SELECT email, pass FROM users WHERE email='$email' AND pass='$pass'";
+        $qry="SELECT service FROM request where service ='service 2'";
+            $result= mysqli_query($con,$qry);
+            if(mysqli_num_rows($result)>0){
+            
+                        echo "congratulation , login successful <br>";
+                        header('refresh:0.0000001;url=displayservice.php');
+                        
+        }
+            else{
+                echo "<h3>please check your inputs and try again<br>Wrong Password or Email!!</h3>";
+            }
+    }
+    else{
+        echo "error";
+    }
+
+    
+
+
+ ?>
